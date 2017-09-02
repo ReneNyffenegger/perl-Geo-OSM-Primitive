83aca892cb526be323c221da1702c313379e62a4
René Nyffenegger <mail@renenyffenegger.ch>
2017-09-02 09:21:12 +0200

    Intitial checkin

---
+++ lib/Geo/OSM/Primitive/Way.pm
@@ -0,0 +1,85 @@
+# Encoding and name #_{
+
+=encoding utf8
+=head1 NAME
+
+Geo::OSM::Primitive::Way - Abstract base class for the Open Street Map data primitive I<way>.
+
+=cut
+package Geo::OSM::Primitive::Way;
+#_}
+#_{ use …
+use warnings;
+use strict;
+
+use utf8;
+use Carp;
+
+#_}
+our $VERSION = 0.01;
+#_{ Synopsis
+
+=head1 SYNOPSIS
+
+    …
+
+=cut
+#_}
+#_{ Overview
+
+=head1 OVERVIEW
+
+The idea is to encapsulte methods that use OpenStreetMap data (that is possibly stored in L<Geo::OSM::DBI>.
+
+=cut
+
+#_}
+#_{ Methods
+
+=head1 METHODS
+=cut
+
+sub new { #_{
+#_{ POD
+
+=head2 new
+
+    new($osm_way_id);
+
+=cut
+
+#_}
+
+  my $class = shift;
+  my $id    = shift;
+
+  my $self = $class->SUPER::new($id);
+
+  croak "Wrong class $class" unless $self->isa('Geo::OSM::Primitive::Way');
+
+  return $self;
+
+} #_}
+
+#_}
+#_{ POD: Copyright
+
+=head1 Copyright
+Copyright © 2017 René Nyffenegger, Switzerland. All rights reserved.
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the Artistic License (2.0). You may obtain a
+copy of the full license at: L<http://www.perlfoundation.org/artistic_license_2_0>
+=cut
+
+#_}
+#_{ POD: Source Code
+
+=head1 Source Code
+
+The source code is on L<< github|https://github.com/ReneNyffenegger/perl-Geo-OSM-Primitive >>. Meaningful pull requests are welcome.
+
+=cut
+
+#_}
+
+'tq84';
---
+++ t/001-loat.t
@@ -0,0 +1,9 @@
+use Test::More tests => 3;
+
+use Geo::OSM::Primitive::Node    ;
+use Geo::OSM::Primitive::Way     ;
+use Geo::OSM::Primitive::Relation;
+
+use_ok('Geo::OSM::Primitive::Node'    );
+use_ok('Geo::OSM::Primitive::Way'     );
+use_ok('Geo::OSM::Primitive::Relation');
